a8d593e4622182e63a8676638e85fa88860b0ff5
NuclearMissile <torrific55@gmail.com>
2024-06-24 14:20:14 +0900

    using iterator instead of index for FilterChain

--- autumn-core/src/main/kotlin/org/example/autumn/server/component/FilterChainImpl.kt
+++ autumn-core/src/main/kotlin/org/example/autumn/server/component/FilterChainImpl.kt
@@ -3,15 +3,13 @@ package org.example.autumn.server.component
 import jakarta.servlet.*
 
 class FilterChainImpl(
-    private val filters: List<Filter>, private val servlet: Servlet
+    filters: List<Filter>, private val servlet: Servlet
 ) : FilterChain {
-    private var index = 0
+    private val iter = filters.iterator()
 
     override fun doFilter(request: ServletRequest, response: ServletResponse) {
-        if (index < filters.size) {
-            val current = index
-            index++
-            filters[current].doFilter(request, response, this)
+        if (iter.hasNext()) {
+            iter.next().doFilter(request, response, this)
         } else {
             servlet.service(request, response)
         }
