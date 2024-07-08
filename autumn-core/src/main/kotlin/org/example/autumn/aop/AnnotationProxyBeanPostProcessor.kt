54622a8e0d58972de36c90be673feb3ab6009886
NuclearMissile <torrific55@gmail.com>
2024-07-08 21:55:46 +0900

    Revert "AOP applies to all methods instead of public methods"
    This reverts commit 1d2d87dd020ee450ae838df0e5346441c985ac8b.

--- autumn-core/src/main/kotlin/org/example/autumn/aop/AnnotationProxyBeanPostProcessor.kt
+++ autumn-core/src/main/kotlin/org/example/autumn/aop/AnnotationProxyBeanPostProcessor.kt
@@ -21,7 +21,7 @@ abstract class AnnotationProxyBeanPostProcessor<A : Annotation> : BeanPostProces
             val targetClass = bean!!.javaClass
             logger.atDebug().log("create proxy for bean {}", targetClass.name)
             val proxyClass = byteBuddy.subclass(targetClass, ConstructorStrategy.Default.DEFAULT_CONSTRUCTOR)
-                .method(ElementMatchers.any())
+                .method(ElementMatchers.isPublic())
                 .intercept(InvocationHandlerAdapter.of { _, method, args ->
                     InvocationChain(aopHandlers).invokeChain(bean, method, args)
                 })
