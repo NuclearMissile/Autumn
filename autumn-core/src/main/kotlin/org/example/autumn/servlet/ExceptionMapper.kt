fd7b808fa396d07ffef1bf831405b94f0c6c7e1d
NuclearMissile <torrific55@gmail.com>
2024-11-18 15:07:29 +0900

    support tail / in url

--- autumn-core/src/main/kotlin/org/example/autumn/servlet/ExceptionMapper.kt
+++
@@ -1,8 +0,0 @@
-package org.example.autumn.servlet
-
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
-
-abstract class ExceptionMapper<T : Exception> {
-    abstract fun map(e: T, req: HttpServletRequest, resp: HttpServletResponse)
-}
